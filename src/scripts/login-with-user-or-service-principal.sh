309b726322ef284e9678cfe0367d19a10113009a
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-02-05 11:37:17 -0500

    fix: evaluating received environment variables

--- src/scripts/login-with-user-or-service-principal.sh
+++ src/scripts/login-with-user-or-service-principal.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+AZURE_USERNAME_V="$(eval echo $AZURE_USERNAME)"
+
 TENANT=""
 if [[ -z "$ALTERNATE_TENANT" ]]; then
-  TENANT="--tenant \"$AZURE_TENANT_V\""
+  TENANT="--tenant \"$AZURE_TENANT\""
 fi
 set -x
-if [ -n "${AZURE_USERNAME}" ]; then
+if [ -n "${AZURE_USERNAME_V}" ]; then
   echo "User credentials detected; logging in with user"
   az login "$TENANT" \
-    -u "$AZURE_USERNAME" \
+    -u "$AZURE_USERNAME_V" \
     -p "$AZURE_PASSWORD"
 elif [ -n "${AZURE_SP}" ]; then
   echo "Service Principal credentials detected; logging in with Service Principal"
