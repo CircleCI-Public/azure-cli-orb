2740e1fdb67f8441d4ca8d085e85c4b1425e8ce4
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-02-07 11:59:34 -0500

    fix: added bash indirect substitution in login with service principal script

--- src/scripts/login-with-service-principal.sh
+++ src/scripts/login-with-service-principal.sh
@@ -2,7 +2,7 @@
 set -x
 az login \
   --service-principal \
-  --tenant "$AZURE_SP_TENANT" \
-  -u "$AZURE_SP" \
-  -p "$AZURE_SP_PASSWORD"
+  --tenant "${!AZURE_SP_TENANT}" \
+  -u "${!AZURE_SP}" \
+  -p "${!AZURE_SP_PASSWORD}"
 set +x
