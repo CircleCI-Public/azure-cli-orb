ac7a688029c393cb42c108619b8b3fbc042211e5
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-02-06 22:42:28 -0500

    fix: simplifying the script to try three possible alternative combination of parameters

--- src/scripts/login-with-user-or-service-principal.sh
+++ src/scripts/login-with-user-or-service-principal.sh
@@ -2,27 +2,34 @@
 
 set -x
 
-TENANT=""
-if [[ "$ALTERNATE_TENANT" -eq 1 ]]; then
-  TENANT="--tenant ${!$AZURE_TENANT}"
+
+if [[ "${!ALTERNATE_TENANT}" -eq 1 ]]; then
+  az login  \
+      "--tenant ${!$AZURE_TENANT}" \
+      -u "${!AZURE_USERNAME}" \
+      -p "${!AZURE_PASSWORD}"
+    exit 0
 fi
 
 if [ -n "${!AZURE_USERNAME}" ]; then
   echo "User credentials detected; logging in with user"
   az login  \
-    "$TENANT" \
     -u "${!AZURE_USERNAME}" \
     -p "${!AZURE_PASSWORD}"
-elif [ -n "${!AZURE_SP}" ]; then
+  exit 0
+fi
+
+if [ -n "${!AZURE_SP}" ]; then
   echo "Service Principal credentials detected; logging in with Service Principal"
   az login \
     --service-principal \
     --tenant "${!AZURE_SP_TENANT}" \
     -u "${!AZURE_SP}" \
     -p "${!AZURE_SP_PASSWORD}"
-else
-  echo 'Login failed; neither user nor Service Principal credentials were provided'
-  exit 1
+  exit 0
 fi
+
+echo 'Login failed; neither user nor Service Principal credentials were provided'
+exit 1
 
 set +x
