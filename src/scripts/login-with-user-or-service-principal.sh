5834edb04aeb03d4588913340ddde7d5cd22ab17
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-02-06 22:26:16 -0500

    fix: testing indirect substitution at runtime

--- src/scripts/login-with-user-or-service-principal.sh
+++ src/scripts/login-with-user-or-service-principal.sh
@@ -9,30 +9,30 @@ set -x
 #AZURE_SP_PASSWORD="$(eval echo "${AZURE_SP_PASSWORD}" | circleci env subst)"
 #AZURE_SP_TENANT="$(eval echo "${AZURE_SP_TENANT}" | circleci env subst)"
 
-AZURE_USERNAME="${!AZURE_USERNAME}"
-AZURE_PASSWORD="${!AZURE_PASSWORD}"
-AZURE_TENANT="${!AZURE_TENANT}"
-AZURE_SP="${!AZURE_SP}"
-AZURE_SP_PASSWORD="${!AZURE_SP_PASSWORD}"
-AZURE_SP_TENANT="${!AZURE_SP_TENANT}"
+#AZURE_USERNAME="${!AZURE_USERNAME}"
+#AZURE_PASSWORD="${!AZURE_PASSWORD}"
+#AZURE_TENANT="${!AZURE_TENANT}"
+#AZURE_SP="${!AZURE_SP}"
+#AZURE_SP_PASSWORD="${!AZURE_SP_PASSWORD}"
+#AZURE_SP_TENANT="${!AZURE_SP_TENANT}"
 
 #TENANT=""
 #if [[ "$ALTERNATE_TENANT" -eq 1 ]]; then
 #  TENANT="--tenant \"$AZURE_TENANT\""
 #fi
 
-if [ -n "${AZURE_USERNAME}" ]; then
+if [ -n "${!AZURE_USERNAME}" ]; then
   echo "User credentials detected; logging in with user"
   az login  \
-    -u "$AZURE_USERNAME" \
-    -p "$AZURE_PASSWORD"
-elif [ -n "${AZURE_SP}" ]; then
+    -u "${!AZURE_USERNAME}" \
+    -p "${!AZURE_PASSWORD}"
+elif [ -n "${!AZURE_SP}" ]; then
   echo "Service Principal credentials detected; logging in with Service Principal"
   az login \
     --service-principal \
-    --tenant "$AZURE_SP_TENANT" \
-    -u "$AZURE_SP" \
-    -p "$AZURE_SP_PASSWORD"
+    --tenant "${!AZURE_SP_TENANT}" \
+    -u "${!AZURE_SP}" \
+    -p "${!AZURE_SP_PASSWORD}"
 else
   echo 'Login failed; neither user nor Service Principal credentials were provided'
   exit 1
