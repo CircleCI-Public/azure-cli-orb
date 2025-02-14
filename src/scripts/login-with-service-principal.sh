5bf708772849ab58fca57acd0c0c27bfbe0a3aa9
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-02-14 09:13:28 -0500

    fix: checking if bash is installed

--- src/scripts/login-with-service-principal.sh
+++ src/scripts/login-with-service-principal.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-az login \
-  --service-principal \
-  --tenant="${!AZURE_SP_TENANT}" \
-  -u="${!AZURE_SP}" \
-  -p="${!AZURE_SP_PASSWORD}"
+if bash --version &>/dev/null; then
+  az login \
+    --service-principal \
+    --tenant="${!AZURE_SP_TENANT}" \
+    -u="${!AZURE_SP}" \
+    -p="${!AZURE_SP_PASSWORD}"
+else
+  echo "Bash not found and is required for azure cli"
+fi
