ce682a3139797aa1b496b69df5ee784b0c432eb1
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-02-14 09:18:40 -0500

    fix: make sure bash is being used

--- src/scripts/login-with-service-principal.sh
+++ src/scripts/login-with-service-principal.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if bash --version &>/dev/null; then
-  az login \
-    --service-principal \
-    --tenant="${!AZURE_SP_TENANT}" \
-    -u="${!AZURE_SP}" \
-    -p="${!AZURE_SP_PASSWORD}"
-else
-  echo "Bash not found and is required for azure cli"
+if [ ! "$BASH_VERSION" ]; then
+  exec /bin/bash "$0" "$@"
 fi
+
+az login \
+  --service-principal \
+  --tenant="${!AZURE_SP_TENANT}" \
+  -u="${!AZURE_SP}" \
+  -p="${!AZURE_SP_PASSWORD}"
