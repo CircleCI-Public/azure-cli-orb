ce061a3aaf521fabef59e98dff3e976e2f709100
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-02-17 23:57:20 -0500

    fix: missed one indirect expansion to fix

--- src/scripts/login-with-user.sh
+++ src/scripts/login-with-user.sh
@@ -6,16 +6,16 @@ AZURE_PASSWORD="$(circleci env subst "$ALTERNATE_TENANT")"
 AZURE_TENANT="$(circleci env subst "$ALTERNATE_TENANT")"
 
 if [[ ! "${ALTERNATE_TENANT}" = "false" ]]; then
-  az login  \
-      "--tenant=${$AZURE_TENANT}" \
-      -u="${AZURE_USERNAME}" \
-      -p="${AZURE_PASSWORD}"
-    exit 0
+  az login \
+    --tenant="${AZURE_TENANT}" \
+    -u="${AZURE_USERNAME}" \
+    -p="${AZURE_PASSWORD}"
+  exit 0
 fi
 
 if [ -n "${AZURE_USERNAME}" ]; then
   echo "User credentials detected; logging in with user"
-  az login  \
+  az login \
     -u="${AZURE_USERNAME}" \
     -p="${AZURE_PASSWORD}"
   exit 0
